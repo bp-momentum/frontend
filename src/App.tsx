20df38202bf3ddba7a7498c95f68f49ed0f1b8c7
Julian Imhof <julianimhof00@gmail.com>
2021-11-12 17:09:52 +0100

    chore: switches to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
+function App() : JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Simple React
         </a>
       </header>
     </div>
