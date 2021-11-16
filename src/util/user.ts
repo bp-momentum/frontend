8df0cec421e8cbf0b8146ef38749176927c0a08d
Laennart <lennart.schmidt@gmx.de>
2021-11-16 18:11:22 +0100

    wip: fix line breakings

--- src/util/user.ts
+++ src/util/user.ts
@@ -1,10 +1,8 @@
 
 class User implements IUser {
   name : string;
-  cockSize : number;
   constructor(u : IUser) {
     this.name = u.name;
-    this.cockSize = u.cockSize;
   }
   getName() : string {
     return this.name;
@@ -14,15 +12,12 @@ class User implements IUser {
 const getUser = () : User => {
   const userObj = {
     name: "Yannik",
-    cockSize: 1
   };
-  
   return new User(userObj);
 };
 
 export interface IUser {
   name : string;
-  cockSize : number;
 }
 
 export default User;
