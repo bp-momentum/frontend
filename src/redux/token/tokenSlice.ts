6fd15eb6a52640729776528e3d1d6e148d20b383
Julian Imhof <julianimhof00@gmail.com>
2021-11-23 23:32:51 +0100

    fix(api): fixed api access to auth token

--- src/redux/token/tokenSlice.ts
+++ src/redux/token/tokenSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import api from "../../util/api";
 
 interface TokenState {
   token: string | null;
@@ -14,9 +15,11 @@ export const tokenSlice = createSlice({
   reducers: {
     setToken: (state, action: PayloadAction<string>) => {
       state.token = action.payload;
+      api.setToken(action.payload);
     },
     unsetToken: (state) => {
       state.token = null;
+      api.setToken("");
     }
   }
 });
