f65ea7e8ffc854e2ab246b413d9f66631663c45f
Laennart <lennart.schmidt@gmx.de>
2021-11-27 20:07:54 +0100

    wip: add test for helper

---
+++ src/util/helper.test.ts
@@ -0,0 +1,6 @@
+import Helper from "./helper";
+
+test("extract account type from a token", () => {
+  const token = "abc.eyJhY2NvdW50X3R5cGUiOiJhZG1pbiIsInRva2VudGltZSI6MTYzODAzMjQyOSwidXNlcm5hbWUiOiJhZG1pbiJ9.def";
+  expect(Helper.getAccountType(token)).toBe("admin");
+});
