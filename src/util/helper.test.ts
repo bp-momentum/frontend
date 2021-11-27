6143543915a5d910808e2cc8648314bd779678b6
Laennart <lennart.schmidt@gmx.de>
2021-11-27 20:12:42 +0100

    test push

--- src/util/helper.test.ts
+++ src/util/helper.test.ts
@@ -1,6 +1,6 @@
 import Helper from "./helper";
 
-test("extract account type from a token", () => {
+test("extract account type from an access token", () => {
   const token = "abc.eyJhY2NvdW50X3R5cGUiOiJhZG1pbiIsInRva2VudGltZSI6MTYzODAzMjQyOSwidXNlcm5hbWUiOiJhZG1pbiJ9.def";
   expect(Helper.getAccountType(token)).toBe("admin");
 });
