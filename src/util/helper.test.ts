490a6a9295f0a24f02fa96363c26b78d864987be
Laennart <lennart.schmidt@gmx.de>
2021-11-27 18:14:46 +0100

    wip: test failing test

--- src/util/helper.test.ts
+++ src/util/helper.test.ts
@@ -2,7 +2,7 @@ import Helper from "./helper";
 
 test("extracts account type from a token", () => {
   const token = "eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X3R5cGUiOiJhZG1pbiIsInRva2VudGltZSI6MTYzODAzMjQyOSwidXNlcm5hbWUiOiJhZG1pbiJ9.pwkoXtEqPuQ8khoRvATUS4xg-sAtmqMcP3zbTo5k9Yc";
-  expect(Helper.getAccountType(token)).toBe("admin");
+  expect(Helper.getAccountType(token)).toBe("trainer");
 });
 
 export {};
