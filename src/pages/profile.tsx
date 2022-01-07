9b4e8b47eed6b7e6e4896d25465b10ac150ca07f
Laennart <lennart.schmidt@gmx.de>
2022-01-07 14:47:05 +0100

    wip: design

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Container from "../shared/container";
-import { Card, Col, Image, Layout, Row } from "antd";
+import { Avatar, Card, Col, Layout, Row } from "antd";
 import { Content } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import { useAppSelector } from "../redux/hooks";
 import Helper from "../util/helper";
+import ReactCardFlip from "react-card-flip";
 
 const Profile = (): JSX.Element => {
   const { t, i18n } = useTranslation();
   const token = useAppSelector((state) => state.token.token);
+  const [flipped, setFlipped] = React.useState<boolean>(false);
 
   return (
     <Container currentPage="profile" color="blue">
@@ -34,7 +36,47 @@ const Profile = (): JSX.Element => {
           <Content>
             <Row gutter={16} justify="space-around">
               <Col className="gutter-row" span={10}>
-                <Card>Jürgen</Card>
+                <ReactCardFlip
+                  isFlipped={flipped}
+                  flipDirection="horizontal"
+                  flipSpeedBackToFront={1.0}
+                  flipSpeedFrontToBack={1.0}
+                >
+                  <Card
+                    onClick={() => setFlipped(true)}
+                    style={{
+                      borderRadius: "5px",
+                      borderColor: "black",
+                      backgroundColor: "#EDEDF4",
+                    }}
+                  >
+                    <Row>
+                      <Avatar
+                        shape="circle"
+                        style={{ backgroundColor: "#626FE5" }}
+                        src="https://cdn.geoscribble.de/avatars/boy_1.png"
+                        size={100}
+                      />
+                      <Col style={{ justifyContent: "center" }}>
+                        <p style={{ fontSize: 24 }}>
+                          {Helper.getUserName(token ?? "")}
+                        </p>
+                        <p>Fleißig seit 2 Monaten</p>
+                      </Col>
+                    </Row>
+                  </Card>
+
+                  <Card
+                    onClick={() => setFlipped(false)}
+                    style={{
+                      borderRadius: "5px",
+                      borderColor: "black",
+                      backgroundColor: "#EDEDF4",
+                    }}
+                  >
+                    Rückseite
+                  </Card>
+                </ReactCardFlip>
               </Col>
               <Col className="gutter-row" span={10}>
                 <div style={{ backgroundColor: "red" }}>col-6</div>
