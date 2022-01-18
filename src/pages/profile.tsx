783f65092643eab0002116b68ee3de0290a8365c
Laennart <lennart.schmidt@gmx.de>
2022-01-18 17:15:27 +0100

    fix: only display current week activity

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -23,6 +23,30 @@ import api from "../util/api";
 import Routes from "../util/routes";
 import ExerciseCache from "../util/exercise_cache";
 
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.com/how-to/javascript
+
+// Returns the ISO week of the date.
+const getWeek = function (d: Date) {
+  const date = new Date(d.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  const week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
+};
+
 const RatingStars = (props: { rating: number }): JSX.Element => {
   return (
     <Row style={{ alignItems: "end" }} justify="space-around">
@@ -680,7 +704,10 @@ const Profile = (): JSX.Element => {
                       .toLocaleDateString("en-GB", { weekday: "long" })
                       .toLowerCase();
                     const doneExercises = profileData?.doneExercises.filter(
-                      (e) => e.done && e.date === dayName
+                      (e) =>
+                        e.done &&
+                        e.date === dayName &&
+                        getWeek(date.toDate()) === getWeek(new Date())
                     );
                     const text = (
                       <Text
