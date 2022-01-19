726297d52f0433756d52cebf64fee4593026632b
Laennart <lennart.schmidt@gmx.de>
2022-01-19 15:33:07 +0100

    fix: nicer colors

--- src/pages/profile/widgets/cards/activity_calendar_card.tsx
+++ src/pages/profile/widgets/cards/activity_calendar_card.tsx
@@ -16,13 +16,13 @@ const ActivityCalendarCard = (props: {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      hash = hash & hash;
     }
-    let color = "#";
-    for (let i = 0; i < 3; i++) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += value.toString(16).substring(-2);
+    if (hash < 0) {
+      hash *= -1;
     }
-    return color;
+    const mod = hash % 0xffffff;
+    return "#" + mod.toString(16);
   };
 
   return (
