e1efc9eb31cb2de7826a5f231f3cdeb71b185931
Laennart <lennart.schmidt@gmx.de>
2022-01-27 13:51:18 +0100

    test: cancellation

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -94,7 +94,7 @@ const Day = forwardRef(
           className={past ? "past" : ""}
           bordered={false}
           style={{
-            borderRadius: "25px",
+            borderRadius: "24px",
             minWidth: "280px",
             background: past ? "#EBEBEB" : "#fff",
             boxShadow: "-2px 4px 4px rgba(0, 0, 0, 0.25)",
