f8bc8321dc6bb5f8d07386c6e27080691469aa46
Laennart <lennart.schmidt@gmx.de>
2022-03-05 17:18:09 +0100

    feat: only show email/phone when set

--- src/pages/profile/user/components/cards/trainer_card.tsx
+++ src/pages/profile/user/components/cards/trainer_card.tsx
@@ -35,17 +35,21 @@ const TrainerCard: React.FC<trainerCardProps> = ({ ...props }) => {
           <br />
           {props.address.replaceAll(", ", "\n")}
           <br />
-          <ButtonContact
-            type={ContactType.phone}
-            contact={props.phone}
-            label={props.phone}
-          />
+          {props.phone.length > 0 && (
+            <ButtonContact
+              type={ContactType.phone}
+              contact={props.phone}
+              label={props.phone}
+            />
+          )}
           <br />
-          <ButtonContact
-            type={ContactType.email}
-            contact={props.email}
-            label={props.email}
-          />
+          {props.email.length > 0 && (
+            <ButtonContact
+              type={ContactType.email}
+              contact={props.email}
+              label={props.email}
+            />
+          )}
         </Text>
       </Row>
     </Card>
