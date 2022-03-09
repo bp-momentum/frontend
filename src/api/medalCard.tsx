a66291240326afac5bd6d0c48634e4bfca438110
Laennart <lennart.schmidt@gmx.de>
2022-03-09 19:08:05 +0100

    feat: add medal card

---
+++ src/api/medalCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "@/pages/profile/user/sub_pages/friends/components/container";
+
+interface medalCardProps {
+  type: "gold" | "silver" | "bronze";
+  count: number;
+  exercise: string;
+}
+
+const MedalCard: React.FC<medalCardProps> = ({ ...props }) => {
+  const { type, count, exercise } = props;
+
+  return (
+    <Container size={{ width: "280px", height: "90px" }}>
+      <span
+        style={{
+          textOverflow: "ellipsis",
+          width: "140px",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+        }}
+      >
+        {count + "x " + exercise + " " + type}
+      </span>
+    </Container>
+  );
+};
+
+export default MedalCard;
--- src/pages/profile/user/sub_pages/achievements/components/medals.tsx
+++ src/pages/profile/user/sub_pages/achievements/components/medals.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import useApi from "@hooks/api";
-import { message } from "antd";
+import { Col, message, Row } from "antd";
 import Routes from "@util/routes";
 import Translations from "@localization/translations";
 import { useTranslation } from "react-i18next";
+import { Medal } from "@api/medal";
+import MedalCard from "@api/medalCard";
 
 const Medals: React.FC = () => {
   const api = useApi();
   const { t } = useTranslation();
+
+  const [medals, setMedals] = React.useState<Medal[]>([]);
 
   const loadMedals = async () => {
     const response = await api.execute(Routes.getMedals());
@@ -17,14 +21,50 @@ const Medals: React.FC = () => {
       );
       return;
     }
-    console.log(response); // TODO remove
+    setMedals(response.data.medals);
   };
   useEffect(() => {
     loadMedals().catch(message.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <>Medals</>;
+  return (
+    <Row>
+      {medals
+        .filter((medal) => medal.gold > 0)
+        .map((medal) => (
+          <Col key={medal.exercise + "-gold"}>
+            <MedalCard
+              type="gold"
+              exercise={medal.exercise}
+              count={medal.gold}
+            />
+          </Col>
+        ))}
+      {medals
+        .filter((medal) => medal.silver > 0)
+        .map((medal) => (
+          <Col key={medal.exercise + "-silver"}>
+            <MedalCard
+              type="silver"
+              exercise={medal.exercise}
+              count={medal.silver}
+            />
+          </Col>
+        ))}
+      {medals
+        .filter((medal) => medal.bronze > 0)
+        .map((medal) => (
+          <Col key={medal.exercise + "-bronze"}>
+            <MedalCard
+              type="bronze"
+              exercise={medal.exercise}
+              count={medal.bronze}
+            />
+          </Col>
+        ))}
+    </Row>
+  );
 };
 
 export default Medals;
