a1c838078c1368b69d32f6611a32c6e807864d4a
Laennart <lennart.schmidt@gmx.de>
2022-03-14 15:49:23 +0100

    fix: display correct language when opening settings for the first time

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -167,7 +167,12 @@ const Settings: React.FC = () => {
             </Divider>
 
             <Row justify="center">
-              <Select defaultValue={i18n.language} onChange={changeLanguage}>
+              <Select
+                defaultValue={
+                  i18n.language.toLowerCase().includes("de") ? "de" : "en"
+                }
+                onChange={changeLanguage}
+              >
                 <Select.Option value="de" key="de">
                   Deutsch
                 </Select.Option>
