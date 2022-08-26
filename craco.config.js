ff740e666eb3009b76b5fc138d557f023c3fef80
Julian Imhof <julianimhof00@gmail.com>
2022-08-26 16:22:18 +0200

    :bug:: fix webpack middleware warning

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
 const path = require("path");
+const evalSourceMap = require("react-dev-utils/evalSourceMapMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+const noopServiceWorker = require("react-dev-utils/noopServiceWorkerMiddleware");
 
 module.exports = {
   webpack: {
@@ -21,5 +25,27 @@ module.exports = {
         ignoreWarnings: [/Failed to parse source map/],
       };
     },
+  },
+  // https://github.com/facebook/create-react-app/issues/12035#issuecomment-1149289491
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
+        middlewares.push(
+          evalSourceMap(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorker(paths.publicUrlOrPath)
+        );
+        return middlewares;
+      },
+    };
+    return devServerConfig;
   },
 };
